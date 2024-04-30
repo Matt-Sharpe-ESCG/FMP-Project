0b30c024270cceb49b5347d8f048d862273f4394
Matthew Sharpe <541704@escg.ac.uk>
2024-04-30 15:57:00 +0100

    M30/04/24

---
+++ Assets/Teleport_Pad.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport_Pad : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
---
+++ Assets/ThirdPersonMovement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    // Gamer Scene Refrences
+    public CharacterController controller;
+    public Transform cam;
+    public Transform groundCheckMarker;
+
+    // Game Asset Values
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    public float gravity = -9.81f;
+    public float groundDistance = 0.4f;
+    public float jumpHeight = 3f;
+    public LayerMask groundMask;
+
+    float turnSmoothVelocity;
+    Vector3 velocity;
+    bool isGrounded; 
+
+    void Update()
+    {
+        // Gravity
+        isGrounded = Physics.CheckSphere(groundCheckMarker.position, groundDistance, groundMask);
+        velocity.y += gravity * Time.deltaTime;
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        // Movement Input
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        // 2 Axis Movement
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+        // Jumping
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+}
